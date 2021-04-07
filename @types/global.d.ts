037c9161a46dc78041b43ed9bae5039d2c482208
David Wheatley <hi@davwheat.dev>
2021-04-07 03:17:37 +0100

    Add more types

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,6 +1,29 @@
 declare global {
+  export interface ControlVisibility {
+    backUp: boolean
+    select: boolean
+
+    red: boolean
+    green: boolean
+    yellow: boolean
+    blue: boolean
+  }
+
+  export type Control = keyof ControlVisibility
+
+  export type SkyControlPressedEvent = CustomEvent<{ control: Control }>
+
   interface Window {
     __bgAudio: HTMLAudioElement
+
+    __shownControls: ControlVisibility
+
+    /**
+     * Sets a control's visibility.
+     *
+     * If `visible` is omitted, it will toggle.
+     */
+    __setControlVisibility: (control: Control, visible?: boolean) => void
   }
 }
 

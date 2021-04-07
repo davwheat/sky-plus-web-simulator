e818601154ce4f6ca6279894483bc4570ca73c33
David Wheatley <hi@davwheat.dev>
2021-04-07 03:19:57 +0100

    Add ControlText for items such as "BACK UP"

---
+++ src/components/ControlText.tsx
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+import Colors from '../data/colors'
+
+const useStyles = makeStyles({
+  root: {
+    border: '1px solid #fff',
+    background: Colors.controlBg,
+    color: '#000',
+    padding: '2px 4px',
+    fontFamily: 'ZurichBT',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+})
+
+const ControlText: React.FC = ({ children }) => {
+  const classes = useStyles()
+
+  return <span className={classes.root}>{children}</span>
+}
+
+export default ControlText

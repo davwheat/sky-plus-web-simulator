ba21cb2cae744aaf5d90dbe50fc11f74de8ca558
David Wheatley <hi@davwheat.dev>
2021-04-09 16:54:39 +0100

    Theme 404 page to make it like a Sky+ box error

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FullScreenError from '../components/FullScreenError'
+import InnerLayout from '../layouts/InnerLayout'
+import { navigate } from 'gatsby'
+
+const Error404: React.FC = () => {
+  return (
+    <InnerLayout>
+      <FullScreenError errorCode={404} backUpPrompt backUpPromptTextAction="go home" onBackUp={() => navigate('/')}>
+        This page doesn't exist on the Sky Web EPG. Maybe this page was removed.
+      </FullScreenError>
+    </InnerLayout>
+  )
+}
+
+export default Error404

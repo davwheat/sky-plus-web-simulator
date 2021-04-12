a0a84cad88b3cf70d606f3ef20dfd612cb409d42
David Wheatley <hi@davwheat.dev>
2021-04-12 02:11:29 +0100

    Add all main EPG menu items to data file

---
+++ src/components/epg/Menus/menuItems.ts
@@ -0,0 +1,144 @@
+import { ListItem } from '../Menu'
+
+export const tvGuideListItems: ListItem[] = [
+  {
+    text: 'All Channels',
+    onClick: () => {},
+  },
+  {
+    text: 'Entertainment',
+    onClick: () => {},
+  },
+  {
+    text: 'Lifestyle & Culture',
+    onClick: () => {
+      alert('l&c')
+    },
+  },
+  {
+    text: 'Movies',
+    onClick: () => {},
+  },
+  {
+    text: 'Sports',
+    onClick: () => {},
+  },
+  {
+    text: 'News',
+    onClick: () => {},
+  },
+  {
+    text: 'Documentaries',
+    onClick: () => {},
+  },
+  {
+    text: 'Kids',
+    onClick: () => {},
+  },
+  {
+    text: 'Music',
+    onClick: () => {},
+  },
+  {
+    text: 'Radio',
+    onClick: () => {},
+  },
+  {
+    text: 'Shopping',
+    onClick: () => {},
+  },
+  {
+    text: 'Religion',
+    onClick: () => {},
+  },
+  {
+    text: 'International',
+    onClick: () => {},
+  },
+  {
+    text: 'Gaming & Dating',
+    onClick: () => {},
+  },
+  {
+    text: 'Specialist',
+    onClick: () => {},
+  },
+  {
+    text: 'Adult',
+    onClick: () => {},
+  },
+]
+
+export const boxOfficeListItems: ListItem[] = [
+  {
+    text: 'Movies by start time',
+    onClick: () => {},
+  },
+  {
+    text: 'Movies A–Z',
+    onClick: () => {},
+  },
+  {
+    text: 'New movies',
+    onClick: () => {},
+  },
+  {
+    text: 'Sports & Events',
+    onClick: () => {},
+  },
+  {
+    text: 'Previews',
+    onClick: () => {},
+  },
+  {
+    text: 'Adult Pay–Per–Night',
+    onClick: () => {},
+  },
+  {
+    text: 'Adult movies',
+    onClick: () => {},
+  },
+]
+
+export const servicesListItems: ListItem[] = [
+  {
+    text: 'Using Sky+',
+    onClick: () => {},
+  },
+  {
+    text: 'Telephone numbers',
+    onClick: () => {},
+  },
+  {
+    text: 'Parental control',
+    onClick: () => {},
+  },
+  {
+    text: 'System setup',
+    onClick: () => {},
+  },
+  {
+    text: 'Sky+ Setup',
+    onClick: () => {},
+  },
+  {
+    text: 'Anytime Setup',
+    onClick: () => {},
+  },
+  {
+    text: 'Auto Standby',
+    onClick: () => {},
+  },
+  {
+    text: 'Manual Recording',
+    onClick: () => {},
+  },
+  {
+    text: 'Favourite Channels',
+    onClick: () => {},
+  },
+  {
+    text: 'Other Channels',
+    onClick: () => {},
+  },
+]

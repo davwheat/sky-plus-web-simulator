34891e2c5d7a475d47d512c85bc84b63e060b92f
David Wheatley <hi@davwheat.dev>
2021-04-12 02:13:51 +0100

    Update global typings

--- @types/global/index.d.ts
+++ @types/global/index.d.ts
@@ -1,9 +1,26 @@
-import type { Control } from '../../src/atoms/controlsState'
-
 declare global {
-  export type SkyControlPressedEvent = CustomEvent<{ control: Control }>
+  export type SkyControlPressedEvent = CustomEvent<{ control: SkyControl }>
 
   export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[] ? ElementType : never
+
+  export type SkyColorButton = 'red' | 'green' | 'yellow' | 'blue'
+
+  export interface ControlVisibility {
+    backUp: boolean
+    select: boolean
+
+    red: boolean
+    green: boolean
+    yellow: boolean
+    blue: boolean
+
+    upArrow: boolean
+    downArrow: boolean
+    leftArrow: boolean
+    rightArrow: boolean
+  }
+
+  export type SkyControl = keyof ControlVisibility
 
   interface Window {
     __bgAudio: HTMLAudioElement

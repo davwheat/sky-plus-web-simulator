d918ee3b0d163e32513357f035b6532c2acf09d9
David Wheatley <hi@davwheat.dev>
2021-04-12 02:10:19 +0100

    Allow an array of SkyControls to be passed to the state setter helper

--- src/helpers/controlsShownStateSetter.ts
+++ src/helpers/controlsShownStateSetter.ts
@@ -1,5 +1,3 @@
-import type { Control } from '../atoms/controlsState'
-
 /**
  * Returns a set state function to be passed into `useSetRecoilState`.
  *
@@ -8,11 +6,29 @@ import type { Control } from '../atoms/controlsState'
  * @example <caption>Shows the back up button</caption>
  * useSetRecoilState(controlsState)(controlsShownStateSetter('backUp', true))
  *
- * @param controlName Name of the control to show/hide/toggle
- * @param isShown (optional) Whether it should be shown or not
+ * @param controlName Name of array of names for the control(s) to show/hide/toggle
+ * @param isShown (optional) Whether it/they should be shown or not
  * @returns Recoil setState handler function to set the visibility of this control
  */
-export default function controlsShownStateSetter(controlName: Control, isShown?: boolean) {
+export default function controlsShownStateSetter(controlName: SkyControl | SkyControl[], isShown?: boolean) {
+  if (Array.isArray(controlName)) {
+    return controlsRaw => {
+      let controls = { ...controlsRaw }
+      console.log('start', controls)
+
+      controlName.forEach(control => {
+        if (Object.keys(controls).includes(control)) {
+          controls[control] = typeof isShown !== 'boolean' ? !controls[control] : isShown
+        } else {
+          throw `Invalid control provided to \`controlsShownStateSetter\`: "${control}".`
+        }
+      })
+
+      console.log(controls)
+      return controls
+    }
+  }
+
   return controlsRaw => {
     let controls = { ...controlsRaw }
     console.log('start', controls)

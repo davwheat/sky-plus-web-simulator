21a3a068fc8ae9be21bf2bd504ac0d9a60582224
David Wheatley <hi@davwheat.dev>
2021-04-12 02:17:41 +0100

    Add ColorButtonsFooter component to show options for coloured buttons

---
+++ src/components/epg/Footer/ColorButtonsFooter.tsx
@@ -0,0 +1,105 @@
+import { makeStyles } from '@material-ui/core'
+import React, { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { controlsState } from '../../../atoms'
+import Colors from '../../../data/Colors'
+import controlsShownStateSetter from '../../../helpers/controlsShownStateSetter'
+import ColorButton from '../../ColorButton'
+
+const useStyles = makeStyles({
+  root: {
+    position: 'absolute',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr',
+    fontFamily: 'ZurichTV',
+    fontSize: 24,
+    letterSpacing: 0.2,
+    color: Colors.accent,
+    bottom: 52,
+    left: 56,
+    right: 28,
+  },
+  colorButton: {
+    lineHeight: 1,
+  },
+})
+
+interface Props {
+  buttonPressHandler: (SkyColorButton) => void
+  buttonsText: Record<SkyColorButton, string>
+}
+
+const ColorButtonsFooter: React.FC<Props> = ({ buttonPressHandler, buttonsText }) => {
+  const classes = useStyles()
+  const setControlsState = useSetRecoilState(controlsState)
+
+  const controlsToEnable: SkyColorButton[] = []
+  const controlsToDisable: SkyColorButton[] = []
+
+  buttonsText['red'] ? controlsToEnable.push('red') : controlsToDisable.push('red')
+  buttonsText['green'] ? controlsToEnable.push('green') : controlsToDisable.push('green')
+  buttonsText['yellow'] ? controlsToEnable.push('yellow') : controlsToDisable.push('yellow')
+  buttonsText['blue'] ? controlsToEnable.push('blue') : controlsToDisable.push('blue')
+
+  setControlsState(controlsShownStateSetter(controlsToEnable, true))
+  setControlsState(controlsShownStateSetter(controlsToDisable, false))
+
+  function buttonPressEventListener(e: SkyControlPressedEvent) {
+    if (['red', 'green', 'yellow', 'blue'].includes(e.detail.control)) {
+      // One of the coloured buttons was pressed
+      e.stopImmediatePropagation()
+      buttonPressHandler(e.detail.control)
+    }
+  }
+
+  // Detect when the coloured buttons are pressed
+  useEffect(() => {
+    document.addEventListener('skyControlPressed', buttonPressEventListener as EventListener)
+
+    return () => {
+      document.removeEventListener('skyControlPressed', buttonPressEventListener as EventListener)
+
+      // Remove the coloured buttons from the available controls when this unmounts
+      setControlsState(controlsShownStateSetter(['red', 'green', 'yellow', 'blue'], false))
+    }
+  })
+
+  return (
+    <footer className={classes.root}>
+      {buttonsText.red ? (
+        <div className={classes.colorButton}>
+          <ColorButton buttonColor="red" />
+          {buttonsText.red}
+        </div>
+      ) : (
+        <div />
+      )}
+      {buttonsText.green ? (
+        <div className={classes.colorButton}>
+          <ColorButton buttonColor="green" />
+          {buttonsText.green}
+        </div>
+      ) : (
+        <div />
+      )}
+      {buttonsText.yellow ? (
+        <div className={classes.colorButton}>
+          <ColorButton buttonColor="yellow" />
+          {buttonsText.yellow}
+        </div>
+      ) : (
+        <div />
+      )}
+      {buttonsText.blue ? (
+        <div className={classes.colorButton}>
+          <ColorButton buttonColor="blue" />
+          {buttonsText.blue}
+        </div>
+      ) : (
+        <div />
+      )}
+    </footer>
+  )
+}
+
+export default ColorButtonsFooter

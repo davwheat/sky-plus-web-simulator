a7518771e78f79d6d575f0bebcd7b9537030098d
David Wheatley <hi@davwheat.dev>
2021-04-12 19:57:41 +0100

    Preload the EPG background image

--- src/html.tsx
+++ src/html.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import EpgBackgroundImage from './assets/images/guide-bg.sized.png'
 
 interface Props {
   htmlAttributes?: Record<string, unknown>
@@ -16,7 +17,10 @@ const HTML: React.FC<Props> = props => {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
         {props.headComponents}
+
+        <link rel="preload" as="image" href={EpgBackgroundImage} />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}

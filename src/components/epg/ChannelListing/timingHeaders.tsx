4fdc03152396ee87e9b0976e59416bc66eca9fcd
David Wheatley <hi@davwheat.dev>
2021-04-14 01:07:30 +0100

    Fix incorrect timing header colour

--- src/components/epg/ChannelListing/timingHeaders.tsx
+++ src/components/epg/ChannelListing/timingHeaders.tsx
@@ -1,9 +1,13 @@
 import { makeStyles } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
+import Colors from '../../../data/Colors'
 import { formatTimingHeader, getScheduleStartTime } from '../../../helpers/timeFormattings'
 
 const useStyles = makeStyles({
   timingHeader: {
+    color: Colors.accent,
+    lineHeight: 1.25,
+
     '&:first-child': { textAlign: 'right' },
     '&:not(:first-child)': {
       gridColumnEnd: 'span 30',

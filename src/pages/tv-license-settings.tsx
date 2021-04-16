ee3085ef63eb9b9a71cfcf806ea23818d6346845
David Wheatley <hi@davwheat.dev>
2021-04-16 04:09:13 +0100

    Add TV License setting page

---
+++ src/pages/tv-license-settings.tsx
@@ -0,0 +1,32 @@
+import { controlsState } from '@atoms/controlsState'
+import TVLicenseMessage from '@components/TVLicenseMessage'
+import controlsShownStateSetter from '@helpers/controlsShownStateSetter'
+import { navigate } from 'gatsby'
+import React, { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
+
+const TVLicenseSettings = () => {
+  const setControlsVisible = useSetRecoilState(controlsState)
+  setControlsVisible(controlsShownStateSetter('backUp', true))
+
+  function goBack() {
+    if (window.__isLastPageOnSameDomain) window.history.back()
+    else navigate('/')
+  }
+
+  function controlPressed(e: SkyControlPressedEvent) {
+    if (e.detail.control === 'backUp') {
+      goBack()
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('skyControlPressed', controlPressed)
+
+    return () => window.removeEventListener('skyControlPressed', controlPressed)
+  })
+
+  return <TVLicenseMessage onSelectLicenseStatus={goBack} />
+}
+
+export default TVLicenseSettings

b84732656d3fcc7e8a89a3e7735d676d658cd7f9
davwheat <hi@davwheat.dev>
2021-04-16 15:21:11 +0000

    Deploying to prod from @ davwheat/sky-plus-web-simulator@66ae45af0541d5e3b2204960bd8127846d4de46e 🚀

---
+++ component---src-pages-404-tsx-35bb9676b7a74f03869f.js
@@ -0,0 +1,2 @@
+(self.webpackChunksky_plus_epg_simulator=self.webpackChunksky_plus_epg_simulator||[]).push([[218],{4909:function(e,t,o){"use strict";o.r(t);var n=o(1733),r=o(5444),s=o(1766),u=o(3230);t.default=function(){return s.default.createElement(u.Z,null,s.default.createElement(n.Z,{errorCode:404,controlPrompt:!0,controlPromptAction:"go home",onControlPressed:function(){return(0,r.navigate)("/")}},"This page doesn't exist on the Sky Web EPG. Maybe this page was removed."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-35bb9676b7a74f03869f.js.map

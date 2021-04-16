47ff0605b76231227658ed4b8cf554410b3232bc
David Wheatley <hi@davwheat.dev>
2021-04-16 05:27:07 +0100

    FIx SSR error where localStorage was accessed during build

--- src/atoms/tvLicenseState.ts
+++ src/atoms/tvLicenseState.ts
@@ -10,16 +10,17 @@ export interface TVLicenseState {
 }
 
 const localStorageEffect = (key: string): AtomEffect<TVLicenseState> => ({ setSelf, onSet }) => {
-  const savedValue = localStorage.getItem(key)
-  if (savedValue != null) {
+  const savedValue = localStorage ? localStorage.getItem(key) : null
+
+  if (savedValue !== null) {
     setSelf(JSON.parse(savedValue))
   }
 
   onSet(newValue => {
     if (newValue instanceof DefaultValue) {
-      localStorage.removeItem(key)
+      localStorage && localStorage.removeItem(key)
     } else {
-      localStorage.setItem(key, JSON.stringify(newValue))
+      localStorage && localStorage.setItem(key, JSON.stringify(newValue))
     }
   })
 }

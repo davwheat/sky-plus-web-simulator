4a4a84435d0266b067d1829e07107cccc0436747
David Wheatley <hi@davwheat.dev>
2021-04-16 04:09:34 +0100

    Add link to TV License settings page

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,8 +1,17 @@
+import { tvLicenseState } from '@atoms/tvLicenseState'
 import chooseMusic, { MusicKeys } from '@data/chooseMusic'
+import shouldShowTvLicenseMessage from '@helpers/shouldShowTvLicenseMessage'
 import { makeStyles } from '@material-ui/core'
+import { Link } from 'gatsby'
 import React from 'react'
+import { useRecoilValue } from 'recoil'
 
 const useStyles = makeStyles({
+  flexDown: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 4,
+  },
   root: {
     position: 'fixed',
     top: 0,
@@ -15,14 +24,22 @@ const useStyles = makeStyles({
     color: '#fff',
     padding: 16,
     paddingTop: 48,
+    gap: 12,
+    width: 250,
     display: 'flex',
     flexDirection: 'column',
-    gap: 4,
+  },
+  tvLicense: {
+    fontSize: 16,
   },
 })
 
 const Settings: React.FC = () => {
   const classes = useStyles()
+  const tvLicenseStateValue = useRecoilValue(tvLicenseState)
+
+  const { hasTvLicense } = tvLicenseStateValue
+  const showTvLicenseWarning = shouldShowTvLicenseMessage(tvLicenseStateValue)
 
   function onBgMusicChange(e: React.ChangeEvent<HTMLSelectElement>) {
     const select = e.target
@@ -38,15 +55,27 @@ const Settings: React.FC = () => {
 
   return (
     <div className={classes.root}>
-      <label htmlFor="settings-bg-music">Background music</label>
-      <select defaultValue={window.localStorage.getItem('epg-music') || 'random'} onChange={onBgMusicChange} id="settings-bg-music">
-        <option value="random">Random</option>
-        {MusicKeys.map(key => (
-          <option key={key} value={key}>
-            {key}
-          </option>
-        ))}
-      </select>
+      <div className={classes.flexDown}>
+        <label htmlFor="settings-bg-music">Background music</label>
+        <select defaultValue={window.localStorage.getItem('epg-music') || 'random'} onChange={onBgMusicChange} id="settings-bg-music">
+          <option value="random">Random</option>
+          {MusicKeys.map(key => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className={classes.tvLicense}>
+        {!showTvLicenseWarning ? (
+          <p>
+            You said that you {hasTvLicense ? 'do' : 'do not'} have a TV License. <Link to="/tv-license-settings">Not right?</Link>
+          </p>
+        ) : (
+          <p>We don't know if you have a TV License.</p>
+        )}
+      </div>
     </div>
   )
 }

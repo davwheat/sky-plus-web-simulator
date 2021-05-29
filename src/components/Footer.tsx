00a412cecc5d96ee5017c04b9fa7f057a1df44d2
David Wheatley <hi@davwheat.dev>
2021-05-29 20:55:42 +0100

    Add noopener to footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,11 +11,11 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
     <footer className={className}>
       Version {version} • Created by{' '}
-      <a target="_blank" href="https://github.com/davwheat">
+      <a target="_blank" rel="noopener" href="https://github.com/davwheat">
         David Wheatley
       </a>{' '}
       •{' '}
-      <a target="_blank" href="https://github.com/davwheat/sky-digibox-web-simulator">
+      <a target="_blank" rel="noopener" href="https://github.com/davwheat/sky-digibox-web-simulator">
         View the source code on GitHub
       </a>
     </footer>
